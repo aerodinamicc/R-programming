a6dfce26bd3648321afbb91470f0464b277c5aef
Z <aerodinamicbg@gmail.com>
2018-12-02 22:37:33 +0100

    high magnitude quakes

--- kaggle/Japan/japan.R
+++ kaggle/Japan/japan.R
@@ -17,9 +17,7 @@ library(stringr)
 #Load data----
 #2016 data
 #V 2.2
-
-dir("gm-jpn-all_u_2_2/gm-jpn-all_u_2_2/", pattern = ".shp")
-
+#BND stands for the administrative boundaries of the 1692 wards in Japan
 bnd <- read_sf("gm-jpn-all_u_2_2/gm-jpn-all_u_2_2/polbnda_jpn.shp")
 
 prefectures <- bnd %>%
@@ -36,16 +34,11 @@ rails <- read_sf("gm-jpn-all_u_2_2/gm-jpn-all_u_2_2/raill_jpn.shp")
 roads <- read_sf("gm-jpn-all_u_2_1/gm-jpn-all_u_2_1/roadl_jpn.shp")
 airport <- read_sf("gm-jpn-all_u_2_2/gm-jpn-all_u_2_2/airp_jpn.shp")
 
-dir("gm-jpn-lu_u_1_1/gm-jpn-lu_u_1_1/jpn")
-
 #Those classifications are from 2006
 #V 1.1
 lu <- raster('gm-jpn-lu_u_1_1/gm-jpn-lu_u_1_1/jpn/lu.tif')
 lc <- raster('gm-jpn-lc_u_1_1/gm-jpn-lc_u_1_1/jpn/lc.tif')
 elev <- raster('gm-jpn-el_u_1_1/gm-jpn-el_u_1_1/jpn/el.tif')
-
-# bnd <- as(bnd, Class = "Spatial")
-# roads <- as(roads, Class = "Spatial")
 
 #Data prep----
 
@@ -193,10 +186,18 @@ eq_input <- read_csv("Japan earthquakes 2001 - 2018.csv") %>%
   dplyr::select(date, year, month, day, longitude, latitude, mag, depth) %>%
   arrange(longitude, latitude)
 
+#High magnitude quakes
+eq_highMag <- eq_input %>%
+  filter(mag > 7)
+
+### 200km Buffer around Japan
+
+#Project all the earthquakes
 eq_input <- st_as_sf(x = eq_input, 
                         coords = c("longitude", "latitude"),
                         crs = "+proj=longlat +datum=WGS84") #
 
+#WGS84
 prefectures <- st_transform(prefectures, 4326)
 
 eq_pref <- eq_input  %>%
@@ -229,6 +230,12 @@ leaflet(prefectures) %>%
               popup=paste(as.character(prefectures$max_mag), prefectures$nam, sep = ", "),
               popupOptions = popupOptions(closeOnClick = TRUE))
 
+#Plot high magnitude quakes
+leaflet(prefectures) %>%
+  addTiles(urlTemplate = "//stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png") %>%
+  addMarkers(lng = eq_highMag$longitude, lat = eq_highMag$latitude,
+             popup = paste(eq_highMag$date, ", ", eq_highMag$mag, " magnitude, ", eq_highMag$depth, " km depth"))
+
 #EQ on Japanese land since 2001
 sum(eq_sf$earthquakes_count)
 
@@ -242,44 +249,7 @@ eq_sf <- bnd %>%
 
 leaflet(sp1) %>%
   addMarkers(lng = eq$longitude, lat = eq$latitude)
-#bnd_joined %>% gather(category, percentage, c("cat10":"cat90")) %>% filter(!is.na(percentage)) %>% group_by(laa) %>% summarise(n = n())
 
-#Two main ways to describe CRS in R are an epsg code or a proj4string
-# definition. Both of these approaches hacrsve advantages and disadvantages.
-# An epsg code is usually shorter, and therefore easier to remember.
-# The code also refers to only one, well-defined coordinate reference system.
-# On the other hand, a proj4string definition allows you more flexibility when
-# it comes to specifying different parameters such as the projection type, the
-# datum and the ellipsoid.14 This way you can specify many different projections,
-# and modify existing ones. This also makes the proj4string approach more
-# complicated. epsg points to exactly one particular CRS.
-
-# Other than searching for EPSG codes online, another quick way to find out about
-# available CRSs is via the rgdal::make_EPSG() function, which outputs a data
-# frame of available projections.
-
-#available CRSs
-#crs_data = rgdal::make_EPSG()
-#View(crs_data)
-
-#tidyverse-pitfalls vignette
-#top_n(n = 3, wt = pop)
-
-#join non spatial datasets to sf objects is the focus of this section
-#left_join() and inner_join() - see vignette("two-table")
-#What if we only want to keep countries that have a match in the key variable? In that case an inner join can be used:
-#world_coffee_inner = inner_join(world, coffee_data)
-#We can identify the rows that did not match using the setdiff()
-
-st_intersects(bnd, airport, sparse = FALSE)
-#st_disjoint(), which returns only objects that do not spatially relate in any way to the selecting object
-
-#st_within() returns TRUE only for objects that are completely within the selecting object. This applies only to the first object, which is inside the triangular polygon
-#st_touches()
-
-#What about features that do not touch, but almost touch the selection object? These can be selected using st_is_within_distance(), which has an additional dist argument.
-
-#zion = st_transform(zion, projection(srtm))
 
   
 
